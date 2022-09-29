2880183175aec80eb05a7b30a88c6477fea997d9
zbaig1912 <bzubair1912@gmail.com>
2022-09-29 02:14:49 -0400

    Created Product and Transaction with Migrations

--- app/models.py
+++ app/models.py
@@ -7,4 +7,12 @@ class User(models.Model):
     name = models.CharField(max_length=120, blank=True)
     email = models.CharField(max_length=120, blank=True)
     firm = models.ForeignKey(Firm, on_delete=models.CASCADE)
-    
+
+class Product(models.Model):
+    name = models.CharField(max_length=120, blank=True)
+    price = models.IntegerField()
+
+class Transaction(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    total = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
